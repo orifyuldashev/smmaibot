d95bc577614cc38555be97da310c2fabe268b111
Orif Yuldashev <orifyuldashev@Orifs-Air.modem.turktelekom>
2026-04-26 08:34:54 -0700

    fix security

--- mm.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv(dotenv_path=".env")
-
-print("BOT:", os.getenv("BOT_TOKEN"))
-print("OPENAI:", os.getenv("OPENAI_API_KEY"))
-print("ADMIN:", os.getenv("ADMIN_ID"))
